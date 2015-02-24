6179bb102e071217f235f668c617d9b59b3a457d
Alon Dahari <climbinghobo@gmail.com>
2015-02-24 16:00:43 -0700

    add cb to translate function

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -6,7 +6,7 @@ var randomWords = require('random-words');
 module.exports = {
 
   word: '',
-  language: '',
+  languageCode: '',
 
 	index: function(req, res) {
     utils.beglobal.languages.all(function(err, results){
@@ -23,7 +23,17 @@ module.exports = {
 
   startQuiz: function(req, res){
     this.word = randomWords();
-    this.language = req.body.language
+    utils.beglobal.languages.all(function(err, results){
+      var fromEnglish = _.filter(results, function(language){
+        return language.from.name === 'English'
+      });
+      var toLanguages = _.map(fromEnglish, function(language){
+        return language.to;
+      })
+      var language = _.findWhere(toLanguages, {name: req.body.language});
+      this.languageCode = language.code;
+    });
+
     res.render('quiz-question', {
       language: req.body.language,
       word: this.word
@@ -31,6 +41,9 @@ module.exports = {
   },
 
   answerQuiz: function(req, res){
-    var translated = utils.translateWord('eng', this.language, this.word)
+    utils.translateWord('eng', this.languageCode, this.word, function(translated){
+
+      res.send(translated);
+    })
   }
 };
